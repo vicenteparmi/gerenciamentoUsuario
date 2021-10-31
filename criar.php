3e721a7220cddc8a7ea6a8cb92cf88f9dcd82db5
Vicente Kalinoski Parmigiani <vicenteparmigiani@gmail.com>
2021-10-31 11:46:29 -0300

    Criar contatos para usuários

---
+++ criar.php
@@ -0,0 +1,134 @@
+<?php
+
+// Start the session if it hasn't already been started
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+// Get the session variables from the post form if set
+if (isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['telefone'])) {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+
+    // Validate the session variables
+    if (empty($nome) || empty($email) || empty($telefone)) {
+        // Alert
+        echo "<script>alert('Preencha todos os campos!');</script>";
+    }
+
+    // Connect to the database
+    $pdo = new PDO('mysql:host=localhost;dbname=agenda', 'root', '');
+
+    // Check if the contact already exists
+    $sql = "SELECT * FROM contato WHERE ContatoNome = :nome";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':nome', $nome);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+
+    // If the contact already exists, alert
+    if (count($result) > 0) {
+        echo "<script>alert('Contato já existe!');</script>";
+    } else {
+
+        $sql = "INSERT INTO contato (ContatoNome, UserID) VALUES (:nome, :id)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':id', $_SESSION['user']);
+        $stmt->execute();
+
+        // Get the contact ID
+        $sql = "SELECT ContatoID FROM contato WHERE ContatoNome = :nome AND UserID = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':id', $_SESSION['user']);
+        $stmt->execute();
+        $result = $stmt->fetch();
+        $contatoID = $result['ContatoID'];
+
+        // Add the phone number to the database
+        $sql = "INSERT INTO telefone (TelNumero, ContatoID) VALUES (:phone, :userId)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':phone', $telefone);
+        $stmt->bindParam(':userId', $contatoID);
+        $stmt->execute();
+
+        // Add the email to the database
+        $sql = "INSERT INTO email (EmailEnd, ContatoID) VALUES (:email, :userId)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':userId', $contatoID);
+        $stmt->execute();
+
+        // Alert
+        echo "<script>alert('Contato adicionado com sucesso!');</script>";
+
+        // Redirect
+        header("Location: index.php");
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda Pessoal</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-light_green.min.css" />
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+</head>
+<style>
+    /* Align card to the middle */
+    .mdl-card {
+        display: block;
+        position: absolute;
+        top: 40%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
+
+<body>
+    <!-- Always shows a header, even in smaller screens. -->
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <!-- import header -->
+        <?php include 'header.php'; ?>
+        <main class="mdl-layout__content">
+            <div class="page-content">
+                <!-- Create contact screen -->
+                <div class="mdl-grid">
+                    <div class="mdl-cell mdl-cell--12-col">
+                        <div class="mdl-card mdl-shadow--2dp">
+                            <div class="mdl-card__title mdl-color--primary mdl-color-text--white">
+                                <h2 class="mdl-card__title-text">Criar Contato</h2>
+                            </div>
+                            <div class="mdl-card__supporting-text">
+                                <form action="criar.php" method="post">
+                                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                        <input class="mdl-textfield__input" type="text" id="nome" name="nome">
+                                        <label class="mdl-textfield__label" for="nome">Nome</label>
+                                    </div>
+                                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                        <input class="mdl-textfield__input" type="text" id="email" name="email">
+                                        <label class="mdl-textfield__label" for="email">Email</label>
+                                    </div>
+                                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                        <input class="mdl-textfield__input" type="number" id="telefone" name="telefone">
+                                        <label class="mdl-textfield__label" for="telefone">Telefone</label>
+                                    </div>
+                                    <!-- send button -->
+                                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
+                                        Criar
+                                    </button>
+        </main>
+    </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -12,6 +12,19 @@
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-light_green.min.css" />
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 </head>
+<style>
+    /* Center table */
+    .mdl-data-table {
+        margin: auto;
+        margin-top: 5vh;
+        width: 90%;
+    }
+
+    /* Center button on page*/
+    .mdl-card__actions {
+        text-align: center;
+    }
+</style>
 
 <body>
     <!-- Always shows a header, even in smaller screens. -->
@@ -30,31 +43,82 @@
                 $contacts = $stmt->fetchAll();
 
                 // If there are no contacts, show a message as a card
-                echo '<div class="mdl-card mdl-shadow--2dp" style="margin-top:15vh; position:absolute; left: 50%; transform: translate(-50%, 0)">';
-                echo '<div class="mdl-card__title mdl-card--expand">';
-                echo '<h2 class="mdl-card__title-text">Nenhum contato cadastrado</h2>';
-                echo '</div>';
-                echo '<div class="mdl-card__supporting-text">';
-                echo '<p>Você ainda não possui nenhum contato cadastrado.</p>';
-                echo '</div>';
-                // Button to create a contact
-                echo '<div class="mdl-card__actions mdl-card--border">';
-                echo '<a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="create.php">';
-                echo 'Criar contato';
-                echo '</a>';
-                echo '</div>';
+                if ($contacts == null) {
+                    echo '<div class="mdl-card mdl-shadow--2dp">
+                            <div class="mdl-card__title">
+                                <h2 class="mdl-card__title-text">Nenhum contato cadastrado</h2>
+                            </div>
+                            <div class="mdl-card__supporting-text">
+                                <p>Você ainda não possui nenhum contato cadastrado. Clique no botão abaixo para adicionar um novo contato.</p>
+                            </div>
+                            <div class="mdl-card__actions mdl-card--border">
+                                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="newContact.php">
+                                    Adicionar Contato
+                                </a>
+                            </div>
+                        </div>';
+                } else {
+                    echo '<table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+                            <thead>
+                                <tr>
+                                    <th class="mdl-data-table__cell--non-numeric">Nome</th>
+                                    <th class="mdl-data-table__cell--non-numeric">Telefone</th>
+                                    <th class="mdl-data-table__cell--non-numeric">E-mail</th>
+                                    <th class="mdl-data-table__cell--non-numeric">Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>';
+                    // Show all contacts from this user
+                    foreach ($contacts as $contact) {
+                        echo '<tr>';
+                        echo '<td class="mdl-data-table__cell--non-numeric">' . $contact['ContatoNome'] . '</td>';
+                        echo '<td class="mdl-data-table__cell--non-numeric">';
 
-                // Show all contacts from this user
-                foreach ($contacts as $contact) {
-                    echo '<tr>';
-                    echo '<td class="mdl-data-table__cell--non-numeric">' . $contact['name'] . '</td>';
-                    echo '<td class="mdl-data-table__cell--non-numeric">' . $contact['phone'] . '</td>';
-                    echo '<td class="mdl-data-table__cell--non-numeric">' . $contact['email'] . '</td>';
-                    echo '<td class="mdl-data-table__cell--non-numeric">';
-                    echo '<a href="edit.php?id=' . $contact['id'] . '">Editar</a>';
-                    echo '<a href="delete.php?id=' . $contact['id'] . '">Excluir</a>';
-                    echo '</td>';
-                    echo '</tr>';
+                        // Get the phone numbers from the database
+                        $sql = "SELECT * FROM telefone WHERE ContatoID = (:contact)";
+                        $stmt = $pdo->prepare($sql);
+                        $stmt->bindParam(':contact', $contact['ContatoID']);
+                        $stmt->execute();
+                        $phones = $stmt->fetchAll();
+
+                        // Show all phone numbers
+                        foreach ($phones as $phone) {
+                            echo $phone['TelNumero'] . '<br>';
+                        }
+
+                        echo '</td>';
+                        echo '<td class="mdl-data-table__cell--non-numeric">';
+
+                        // Get the emails from the database
+                        $sql = "SELECT * FROM email WHERE ContatoID = (:contact)";
+                        $stmt = $pdo->prepare($sql);
+                        $stmt->bindParam(':contact', $contact['ContatoID']);
+                        $stmt->execute();
+                        $emails = $stmt->fetchAll();
+
+                        // Show all emails
+                        foreach ($emails as $email) {
+                            echo $email['EmailEnd'] . '<br>';
+                        }
+
+                        echo '</td>';
+                        echo '<td class="mdl-data-table__cell--non-numeric">';
+                        echo '<a href="edit.php?id=' . $contact['ContatoID'] . '">Editar</a>';
+                        echo '<a href="delete.php?id=' . $contact['ContatoID'] . '">Excluir</a>';
+                        echo '</td>';
+                        echo '</tr>';   
+                    
+                    }
+
+                    echo '</tbody>
+                    </table>';
+
+                    echo '<div class="mdl-card__actions mdl-card--border">
+                                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="criar.php">
+                                    Adicionar Contato
+                                </a>
+                            </div>';
+
                 }
 
                 ?>
